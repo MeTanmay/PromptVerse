42d61ff54f8e665fd70c383c232ade362602d0f1
Tanmay Borse <tanmayborse28@gmail.com>
2024-08-15 19:07:11 +0530

    Google Auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,13 @@
 "use client"
 
-import Link from 'next/link'
-import Image from 'next/image'
-import {useState, useEffect} from 'react';
-import {signIn, signOut, useSession, getProviders} from 'next-auth/react' 
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn=true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -35,7 +36,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Post
@@ -47,7 +48,7 @@ const Nav = () => {
 
             <Link href='/profile'>
               <Image
-                src='/assets/images/logo.svg'
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -77,15 +78,15 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
-              src='/assets/images/logo.svg'
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
               alt='profile'
-              onClick={() => setToggleDropdown((prev)=> !prev)}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
 
             {toggleDropdown && (
